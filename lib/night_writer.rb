c59c1bd845fc78702bac727c85380ffc70b9b72a
Garret Wolf <garretwolf26@gmail.com>
2020-08-01 11:24:02 -0400

    Initialize input file, output file, and read input in order to access characters in message.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,22 +1,30 @@
 class NightWriter
-  attr_reader :file_reader
+  attr_reader :file_reader, :input_file, :output_file, :read_input
 
   def initialize
-    @reader = FileReader.new
+    # @reader = FileReader.new
+    @input_file = ARGV[0]
+    @output_file = ARGV[1]
+    @read_input = File.read(input_file)
   end
 
-  def encode_file_to_braille
-    # I wouldn't worry about testing this method
-    # unless you get everything else done
-    plain = reader.read
-    braille = encode_to_braille(plain)
-  end
-
-  def encode_to_braille(input)
-    # you've taken in an INPUT string
-    # do the magic
-    # send out an OUTPUT string
-  end
 end
 
-puts ARGV.inspect
+
+# Code from starter file
+#
+#   def encode_file_to_braille
+#     # I wouldn't worry about testing this method
+#     # unless you get everything else done
+#     plain = reader.read
+#     braille = encode_to_braille(plain)
+#   end
+#
+#   def encode_to_braille(input)
+#     # you've taken in an INPUT string
+#     # do the magic
+#     # send out an OUTPUT string
+#   end
+# end
+#
+# puts ARGV.inspect
