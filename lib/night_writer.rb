87877da1dc1554f163ba542b01de46dd78f3283e
Garret Wolf <garretwolf26@gmail.com>
2020-08-01 22:25:55 -0400

    Create method to encode to braille

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,5 @@
+require "./lib/dictionary"
+
 class NightWriter
   attr_reader :file_reader, :input_file, :output_file,
   :read_input, :read_output
@@ -8,6 +10,7 @@ class NightWriter
     @output_file = ARGV[1]
     @read_input = File.read(input_file)
     @read_output = File.read(output_file)
+    @dictionary = Dictionary.new
   end
 
   def save_input_to_output #Saves the message contained in input to the output file
@@ -23,10 +26,27 @@ class NightWriter
     puts "Created '#{output_file}' containing #{count_input_characters} characters"
     #Added gsub to so it doesn't count newline as a character
   end
+
+  def encode_to_braille(input)
+    # you've taken in an INPUT string
+    # do the magic
+    # send out an OUTPUT string - this will be the braille characters
+    line1 = ''
+    line2 = ''
+    line3 = ''
+    input.each_char do |char|
+      line1 += "#{@dictionary.braille_dictionary[input[char]][0..1]}"
+      line2 += "#{@dictionary.braille_dictionary[input[char]][2..3]}"
+      line3 += "#{@dictionary.braille_dictionary[input[char]][4..5]}"
+    end
+    "#{line1}\n#{line2}\n#{line3}" ## spacing in the output may be off, may need to adjust how this string is printed
+  end
+
 end
 
-night_writer = NightWriter.new
-night_writer.print_to_terminal
+
+# night_writer = NightWriter.new
+# night_writer.print_to_terminal
 
 
 # Code from starter file
@@ -38,11 +58,6 @@ night_writer.print_to_terminal
 #     braille = encode_to_braille(plain)
 #   end
 #
-#   def encode_to_braille(input)
-#     # you've taken in an INPUT string
-#     # do the magic
-#     # send out an OUTPUT string
-#   end
 # end
 #
 # puts ARGV.inspect
