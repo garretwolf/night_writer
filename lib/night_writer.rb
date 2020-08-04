7fc644356c0090f002c61e0b441881611efed72f
Garret Wolf <garretwolf26@gmail.com>
2020-08-03 20:08:06 -0400

    Remove comments to clear up code

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -5,7 +5,6 @@ class NightWriter
   :read_input, :read_output
 
   def initialize
-    # @reader = FileReader.new
     @input_file = ARGV[0]
     @output_file = ARGV[1]
     @read_input = File.read(input_file)
@@ -13,7 +12,7 @@ class NightWriter
     @dictionary = Dictionary.new
   end
 
-  def save_input_to_output #Saves the message contained in input to the output file
+  def save_input_to_output
     File.open(output_file, "w") { |file| file.write(read_input) }
   end
 
@@ -22,9 +21,7 @@ class NightWriter
   end
 
   def print_to_terminal
-    # require "pry"; binding.pry
     puts "Created '#{output_file}' containing #{count_input_characters} characters"
-    #Added gsub to so it doesn't count newline as a character
   end
 
   def encode_to_braille(input)
@@ -36,7 +33,7 @@ class NightWriter
       line2 += "#{@dictionary.braille_dictionary[input[char]][2..3]}"
       line3 += "#{@dictionary.braille_dictionary[input[char]][4..5]}"
     end
-    "#{line1}\n#{line2}\n#{line3}" ## spacing in the output may be off, may need to adjust how this string is printed
+    "#{line1}\n#{line2}\n#{line3}"
   end
 
   def limit_characters_per_line(input)
@@ -49,18 +46,3 @@ end
 
 
 night_writer = NightWriter.new
-# night_writer.limit_characters_per_line("helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld")
-
-
-# Code from starter file
-#
-#   def encode_file_to_braille
-#     # I wouldn't worry about testing this method
-#     # unless you get everything else done
-#     plain = reader.read
-#     braille = encode_to_braille(plain)
-#   end
-#
-# end
-#
-# puts ARGV.inspect
